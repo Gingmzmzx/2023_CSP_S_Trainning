c506bb52500779e35cf8c4b45d5578e2a5b65af5
gingmzmzx <2417481092@qq.com>
2023-10-04 11:20:34 +0800

    day5的10.3 slides.pptx是J组刷题营的课件（感谢523大佬提供） 新建day6，pdf晚上发

---
+++ day6/test.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define MAXN 100010
+using namespace std;
+int main(){
+    //	freopen(".in", "r", stdin);
+    //	freopen(".out", "w", stdout);
+    //	freopen(".err", "w", stderr);
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);cerr.tie(0);
+    
+    
+    return 0;
+}
